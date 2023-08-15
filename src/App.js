329cea5763d565b5d4564d9114b0b999c8073662
m-moroishi <m-moroishi@b-tm.co.jp>
2023-08-15 13:41:46 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: '掃除', completed: true },
+    { id: 2, content: '洗濯', completed: true },
+  ]);
+
+  const todoContentRef = useRef();
+
+  const handleAddTodo = () => {
+    const content = todoContentRef.current.value;
+    if (content === '') return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), content: content, completed: false }];
+    });
+
+    todoContentRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoContentRef} />
+      <button onClick={handleAddTodo}>追加</button>
+      <button onClick={handleClear}>完了したタスク</button>
+      <div>残りのタスク{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
